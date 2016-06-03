53b6fab78d818927db43f9e01b792dfeaddccbe6
Tom - Chaplean <tom@chaplean.com>
2016-06-03 17:47:03 +0200

    Upgrade Bundle / Unit 3 / PHP7 compatibility

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -12,6 +12,9 @@ use Symfony\Component\Config\Loader\LoaderInterface;
  */
 class AppKernel extends Kernel
 {
+    /**
+     * @return array
+     */
     public function registerBundles()
     {
         return array(
@@ -22,20 +25,18 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
             new Symfony\Bundle\MonologBundle\MonologBundle(),
-            new Chaplean\Bundle\UnitBundle\ChapleanUnitBundle(),
             new Liip\FunctionalTestBundle\LiipFunctionalTestBundle(),
+            new Chaplean\Bundle\UnitBundle\ChapleanUnitBundle(),
             new Chaplean\Bundle\SocialButtonsBundle\ChapleanSocialButtonsBundle(),
         );
     }
 
     /**
-     * @param LoaderInterface $loader
-     *
-     * @return void
+     * @return string
      */
-    public function registerContainerConfiguration(LoaderInterface $loader)
+    public function getRootDir()
     {
-        $loader->load(__DIR__ . '/config/config_' . $this->getEnvironment() .'.yml');
+        return __DIR__;
     }
 
     /**
@@ -43,12 +44,7 @@ class AppKernel extends Kernel
      */
     public function getCacheDir()
     {
-        $cacheDir = sys_get_temp_dir() . '/cache/' . $this->getEnvironment();
-        if (!is_dir($cacheDir)) {
-            mkdir($cacheDir, 0777, true);
-        }
-
-        return $cacheDir;
+        return dirname(__DIR__) . '/var/cache/' . $this->getEnvironment();
     }
 
     /**
@@ -56,11 +52,16 @@ class AppKernel extends Kernel
      */
     public function getLogDir()
     {
-        $logDir = sys_get_temp_dir() . '/logs/' . $this->getEnvironment();
-        if (!is_dir($logDir)) {
-            mkdir($logDir, 0777, true);
-        }
+        return dirname(__DIR__) . '/var/logs';
+    }
 
-        return $logDir;
+    /**
+     * @param LoaderInterface $loader Resource loader.
+     *
+     * @return void
+     */
+    public function registerContainerConfiguration(LoaderInterface $loader)
+    {
+        $loader->load(__DIR__ . '/config/config_' . $this->getEnvironment() . '.yml');
     }
 }

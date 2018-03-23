e154bca41568efeb3b981cf2093301df74a008ca
Matthias Devlamynck <matthias@chaplean.coop>
2018-03-23 17:27:13 +0100

    #26124, Fix tests, cleanup

---
+++ app/AppKernel.php
@@ -0,0 +1,70 @@
+<?php
+
+// @codingStandardsIgnoreFile
+
+use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\Config\Loader\LoaderInterface;
+
+/**
+ * Class AppKernel.
+ *
+ * @author    Valentin - Chaplean <valentin@chaplean.coop>
+ * @copyright 2014 - 2015 Chaplean (http://www.chaplean.coop)
+ * @since     1.0.0
+ */
+class AppKernel extends Kernel
+{
+    /**
+     * @return array
+     */
+    public function registerBundles()
+    {
+        $bundles = [
+            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
+            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
+            new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
+            new Symfony\Bundle\TwigBundle\TwigBundle(),
+            new Chaplean\Bundle\UnitBundle\ChapleanUnitBundle(),
+            new Liip\FunctionalTestBundle\LiipFunctionalTestBundle(),
+            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
+            new Chaplean\Bundle\SocialButtonsBundle\ChapleanSocialButtonsBundle(),
+        ];
+
+        return $bundles;
+    }
+
+    /**
+     * @return string
+     */
+    public function getRootDir()
+    {
+        return __DIR__;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCacheDir()
+    {
+        return dirname(__DIR__) . '/var/cache/' . $this->getEnvironment();
+    }
+
+    /**
+     * @return string
+     */
+    public function getLogDir()
+    {
+        return dirname(__DIR__) . '/var/logs';
+    }
+
+    /**
+     * @param LoaderInterface $loader Resource loader.
+     *
+     * @return void
+     */
+    public function registerContainerConfiguration(LoaderInterface $loader)
+    {
+        $loader->load($this->getRootDir() . '/config/config.yml');
+    }
+}
